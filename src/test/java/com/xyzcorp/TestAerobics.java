b1d149d782a0941d3ae45b17de06bb0904387fd7
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-02-14 14:46:53 -0500

    Added setup method for aerobics API tests

---
+++ src/test/java/com/xyzcorp/TestAerobics.java
@@ -0,0 +1,15 @@
+package com.xyzcorp;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestAerobics {
+    
+    BasicEndpointFunctions basicEndpointFunctions;
+
+    @BeforeEach
+    void setup() {
+        basicEndpointFunctions = new BasicEndpointFunctions();
+    }
+
+    
+}

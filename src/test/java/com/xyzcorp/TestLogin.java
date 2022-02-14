1460df84bbb083fa02c32cb7767cd574b5512a18
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-02-14 11:47:47 -0500

    Added JSON field assertions for valid login endpoint

--- src/test/java/com/xyzcorp/TestLogin.java
+++ src/test/java/com/xyzcorp/TestLogin.java
@@ -16,15 +16,23 @@ public class TestLogin {
     }
     
     /**
-     * Verify that an existing login endpoint returns status code 200
+     * Verify that an existing login endpoint returns status code 200, and that the JSON string contains the proper username and password
      */
     @Test
     public void testValidLoginEndpoint() {
+        String userNameField = "username", passwordField = "password";
         String userName  = "sallys", password = "point234";
-        String url = String.format("https://staging.tiered-planet.net/werk-it-back-end/login/%s/%s", userName, password);
+        String sutURL = String.format("https://staging.tiered-planet.net/werk-it-back-end/login/%s/%s", userName, password);
         int expectedStatusCode = 200;
 
-        basicEndpointFunctions.verifyGetEndpointStatusCode(url, expectedStatusCode);
+        // Assert that the endpoint is valid
+        basicEndpointFunctions.verifyGetEndpointStatusCode(sutURL, expectedStatusCode);
+        
+        // Assert that the username field has the proper username
+        basicEndpointFunctions.assertEndpointFieldValue(userNameField, userName, sutURL);
+
+        basicEndpointFunctions.assertEndpointFieldValue(passwordField, password, sutURL);
+
     }
 
     /**
@@ -33,10 +41,10 @@ public class TestLogin {
     @Test
     public void testInvalidLoginEndpoint() {
         String userName  = "admin", password = "badclock";
-        String url = String.format("https://staging.tiered-planet.net/werk-it-back-end/login/%s/%s", userName, password);
+        String sutURL = String.format("https://staging.tiered-planet.net/werk-it-back-end/login/%s/%s", userName, password);
         int expectedStatusCode = 401;
 
-        basicEndpointFunctions.verifyGetEndpointStatusCode(url, expectedStatusCode);
+        basicEndpointFunctions.verifyGetEndpointStatusCode(sutURL, expectedStatusCode);
     }
 
 }

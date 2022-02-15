1db9fc3f2d89bbcfd972cac98633604c07764dfe
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-02-15 11:00:44 -0500

    Added areobics invalid post tests

--- src/test/java/com/xyzcorp/TestAerobics.java
+++ src/test/java/com/xyzcorp/TestAerobics.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.Matchers.equalTo;
-
 import java.util.HashMap;
  
 
@@ -28,7 +26,7 @@ public class TestAerobics {
     public void testExistingAerobicsGet() {
         HashMap<String, Object> expectedValuesMap = new HashMap<>();
         expectedValuesMap.put("[0].seconds", 3000);
-        expectedValuesMap.put("[0].name", null);
+        expectedValuesMap.put("[0].name", null); // Name should not be null in the first place
         expectedValuesMap.put("[0].userId", 1);
         expectedValuesMap.put("[0].id", 265);
 
@@ -48,21 +46,45 @@ public class TestAerobics {
         String sutURL = String.format("https://staging.tiered-planet.net/werk-it-back-end/aerobics/user/%d", userId);
         int expectedStatusCode = 200;
 
-        HashMap<String, Object> newValues = new HashMap<>();
-        newValues.put("seconds", 1800);
-        newValues.put("name", "Dance");
+        HashMap<String, Object> newValuesMap = new HashMap<>();
+        newValuesMap.put("seconds", 1800);
+        newValuesMap.put("name", "Dance");
         
-        httpRequestFunctions.assertPostEndpointStatusCode(sutURL, expectedStatusCode, newValues);
+        httpRequestFunctions.assertPostEndpointStatusCode(sutURL, expectedStatusCode, newValuesMap);
     }
 
+    /**
+     * Asserts that a negative time is rejected by the back-end in a POST request
+     */
     @Test 
-    public void testInvalidSecondsPost() {
+    @Disabled("The POST request does not verify for invalid time")
+    public void testNegativeSecondsInvalidPost() {
+        int userId = 1;
+        String sutURL = String.format("https://staging.tiered-planet.net/werk-it-back-end/aerobics/user/%d", userId);
+        int expectedStatusCode = 401;
 
+        HashMap<String, Object> newValuesMap = new HashMap<>();
+        newValuesMap.put("seconds", -1200);
+        newValuesMap.put("name", "Jogging");
+
+        httpRequestFunctions.assertPostEndpointStatusCode(sutURL, expectedStatusCode, newValuesMap);
     }
 
+    /**
+     * Asserts that an empty name is rejected by the back-end in a POST request
+     */
     @Test
-    public void testInvalidNamePost() {
+    @Disabled("The POST request does not verify for invalid name")
+    public void testEmptyNameInvalidPost() {
+        int userId = 1;
+        String sutURL = String.format("https://staging.tiered-planet.net/werk-it-back-end/aerobics/user/%d", userId);
+        int expectedStatusCode = 401;
 
+        HashMap<String, Object> newValuesMap = new HashMap<>();
+        newValuesMap.put("seconds", 300);
+        newValuesMap.put("name", "");
+
+        httpRequestFunctions.assertPostEndpointStatusCode(sutURL, expectedStatusCode, newValuesMap);
     }
 
 }

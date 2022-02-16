c2de4cb5daa8d2c53b19bca780d5a0b007031971
hmaied ben brahem <hmaied.benbrahem@cgi.com>
2022-02-16 11:40:46 -0500

    delete fruit file

--- src/test/java/com/xyzcorp/Apitest.java
+++ src/test/java/com/xyzcorp/Apitest.java
@@ -8,7 +8,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.nullValue;
 
-public class Apitest {
+public class APITest {
 
 
     @Test
